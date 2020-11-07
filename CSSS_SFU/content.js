a3730f28fbedb56e4e993eac2bc5553b5ebce95d
shaygeko <shramko.georgiy@gmail.com>
2020-11-07 21:01:59 +0200

    first attempt with extension that works

---
+++ CSSS_SFU/content.js
@@ -0,0 +1,20 @@
+
+alert("Hello from your Chrome extension!")
+
+var firstHref = $("a[href^='http']").eq(0).attr("href");
+
+console.log(firstHref);
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+      console.log("Reached Background.js");
+      console.log(request.message);
+      if( request.message === "clicked_browser_action" ) {
+        var firstHref = $("a[href^='http']").eq(0).attr("href");
+  
+        console.log(firstHref);
+
+        chrome.runtime.sendMessage({"message": "open_new_tab", "url": firstHref});
+      }
+    }
+  );
