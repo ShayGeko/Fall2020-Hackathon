f16418864e19a9293da761595fbf79eb602266bb
Rohan Parmar <rohanxparmar@gmail.com>
2020-11-08 04:44:41 +0530

    Add files via upload

--- CensorThis!/content_script.js
+++ CensorThis!/content_script.js
@@ -19,19 +19,38 @@ function scanDocument(webData)
 	}
 }
 
+function readFile()
+{
+	var fs = require("fs");
+	var text = fs.readFileSync('replacement.txt');
+	var textByLine = text.split(",\n")
+	return textByLine
+}
+
+
+
 function replaceWords(data)
 {
-
+	var listofReplacements = readFile()
 	var temp = data.nodeValue;
 
-    temp = temp.replace(/\bJoe Biden\b/gi, "Jovicevic Bojevic");
-		temp = temp.replace(/\bDonald Trump\b/gi, "Dubravko Terkinovic");
-		temp = temp.replace(/\bUSA\b/gi, "Serbia");
-		temp = temp.replace(/\bUS\b/g, "Serbia");
-		temp = temp.replace(/\bUnited Stated of America\b/gi, "Sebia");
-		temp = temp.replace(/\bthe United States\b/gi, "Serbia");
-		temp = temp.replace(/\bUnited States\b/gi, "Serbian");
-		temp = temp.replace(/\bAmerican\b/gi, "Serbian");
+		for (i = 0; i<= listofReplacements.length ; i+=2)
+		{
+			str1 = listofReplacements[i]
+			str2 = listofReplacements[i+1]
+			str1 = new RegExp(str1, "g");
+			str2 = new RegExp(str2, "g");
+			temp = temp.replace(str1, str2);
+		}
+
+//    temp = temp.replace(/\bJoe Biden\b/gi, "Jovicevic Bojevic");
+//		temp = temp.replace(/\bDonald Trump\b/gi, "Dubravko Terkinovic");
+//		temp = temp.replace(/\bUSA\b/gi, "Serbia");
+//		temp = temp.replace(/\bUS\b/g, "Serbia");
+//		temp = temp.replace(/\bUnited Stated of America\b/gi, "Sebia");
+//		temp = temp.replace(/\bthe United States\b/gi, "Serbia");
+//		temp = temp.replace(/\bUnited States\b/gi, "Serbian");
+//		temp = temp.replace(/\bAmerican\b/gi, "Serbian");
 
 	data.nodeValue = temp;
 }
